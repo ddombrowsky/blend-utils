a631159e31518ce737873ee370a38bbb03c763db
David Dombrowsky <davek@6thstreetradio.org>
2024-08-08 12:50:22 -0400

    added loop for multiple pool contracts

--- balances/index.ts
+++ balances/index.ts
@@ -5,7 +5,10 @@ const network: Network = {
   passphrase: "Public Global Stellar Network ; September 2015",
 };
 
-const poolContract = 'CDVQVKOY2YSXS2IC7KN6MNASSHPAO7UN2UR2ON4OI2SKMFJNVAMDX6DP';
+const poolContracts = [
+  'CDVQVKOY2YSXS2IC7KN6MNASSHPAO7UN2UR2ON4OI2SKMFJNVAMDX6DP',
+//  'CBP7NO6F7FRDHSOFQBT2L2UWYIZ2PU76JKVRYAQTG3KZSQLYAOKIF2WB'
+];
 const backstopContract = 'CAO3AGAMZVRMHITL36EJ2VZQWKYRPWMQAPDQD5YEOF3GIF7T44U4JAL3';
 
 const replacer = (key: any, value: any) => {
@@ -24,13 +27,18 @@ const replacer = (key: any, value: any) => {
   }
 };
 
-async function display(userPub: string) {
+async function display(userPub: string, poolId: string) {
+  const pool = await Pool.load(
+    network,
+    poolId,
+    Math.floor(Date.now() / 1000));
+
   const user = await pool.loadUser(
     network,
     userPub);
 
   //console.log(JSON.stringify(user, replacer, 2));
-  console.log(`\nBalances for ${user.user} ->`);
+  console.log(`Balances for ${user.user} ->`);
 
   for (let x of user.positionEstimates.liabilities) {
     console.log(x);
@@ -39,37 +47,33 @@ async function display(userPub: string) {
     console.log(x);
   }
 
-  console.log('backstop:');
-  const userBackstop = await BackstopUser.load(
-    network,
-    userPub,
-    backstop);
-
-  //console.log(JSON.stringify(userBackstop, replacer, 2));
-  console.log(userBackstop.estimates.get(poolContract).tokens);
 }
 
-let pool: Pool;
 let backstop: Backstop;
 
 async function main(pubkeys: Array<string>) {
-  pool = await Pool.load(
-    network,
-    poolContract,
-    Math.floor(Date.now() / 1000));
-
   backstop = await Backstop.load(
     network,
     backstopContract,
-    [poolContract],
+    poolContracts,
     true,
     Math.floor(Date.now() / 1000));
 
-  // console.log(JSON.stringify(pool, replacer, 2));
-
   for (let pubkey of pubkeys) {
-    await display(pubkey);
+    console.log(`\n== USER ${pubkey} ==`);
+    const userBackstop = await BackstopUser.load(
+      network,
+      pubkey,
+      backstop);
+
+    for (let poolId of poolContracts) {
+      console.log(`POOL ${poolId}`);
+      await display(pubkey, poolId);
+      console.log('backstop:');
+      console.log(userBackstop.estimates.get(poolId).tokens);
+    }
   }
+
 }
 
 if (process.argv.length < 3) {
