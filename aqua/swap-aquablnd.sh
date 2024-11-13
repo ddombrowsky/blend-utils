a975cb281b5b6956972d1510aa1ec3a2383e6cf5
David Dombrowsky <davek@6thstreetradio.org>
2024-11-13 09:31:06 -0500

    added aqua->blnd swap script

--- aqua/swap-aquablnd.sh
+++ aqua/swap-aquablnd.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 
-# swap BLND -> AQUA in pool
-BLND_N=$1
-# price is in BLND
-# to get 0.0006 per BLND, do 1/0.06 = 1666, = bottom sell price in orderbook
+# swap AQUA -> BLND in pool
+AQUA_N=$1
+# price is in AQUA
 PRICE=$2
 
-# in BLND
-FEEMARGIN=0.15
+# in AQUA
+FEEMARGIN=5
 
-BLND=`echo $BLND_N*10000000|bc|sed -e 's/\..*//'`
-MIN_OUT=`echo $BLND/$PRICE|bc|sed -e 's/\..*//'`
+AQUA=`echo $AQUA_N*10000000|bc|sed -e 's/\..*//'`
+MIN_OUT=`echo $AQUA/$PRICE|bc|sed -e 's/\..*//'`
 
-echo BLND=$BLND
+echo AQUA=$AQUA
 echo MIN_OUT=$MIN_OUT
 echo FEEMARGIN=$FEEMARGIN
 
@@ -22,9 +21,9 @@ export SECRET_KEY=`stellar keys show xbull`
 
 #    BLND/AQUA
 # 0.30%
-CHAIN='[ [ ["CAUIKL3IYGMERDRUN6YSCLWVAKIFG5Q4YJHUKM4S4NJZQIA3BAS6OJPK", "CD25MNVTZDL4Y3XBCPCJXGXATV5WUHHOWMYFF4YBEGU5FCPGMYTVG5JY"], "9ac7a9cde23ac2ada11105eeaa42e43c2ea8332ca0aa8f41f58d7160274d718e", "CAUIKL3IYGMERDRUN6YSCLWVAKIFG5Q4YJHUKM4S4NJZQIA3BAS6OJPK" ] ]'
+CHAIN='[ [ ["CAUIKL3IYGMERDRUN6YSCLWVAKIFG5Q4YJHUKM4S4NJZQIA3BAS6OJPK", "CD25MNVTZDL4Y3XBCPCJXGXATV5WUHHOWMYFF4YBEGU5FCPGMYTVG5JY"], "9ac7a9cde23ac2ada11105eeaa42e43c2ea8332ca0aa8f41f58d7160274d718e", "CD25MNVTZDL4Y3XBCPCJXGXATV5WUHHOWMYFF4YBEGU5FCPGMYTVG5JY" ] ]'
 
-TOKSTR=BLND
+TOKSTR=AQUA
 while [ $good = 1 ] ; do 
     soroban contract invoke \
     --id CBQDHNBFBZYE4MKPWBSJOPIYLW4SFSXAXUTSXJN76GNKYVYPCKWC6QUK \
@@ -33,8 +32,8 @@ while [ $good = 1 ] ; do
     swap_chained \
     --user xbull \
     --swaps_chain "$CHAIN" \
-    --token_in CD25MNVTZDL4Y3XBCPCJXGXATV5WUHHOWMYFF4YBEGU5FCPGMYTVG5JY \
-    --in_amount $BLND \
+    --token_in CAUIKL3IYGMERDRUN6YSCLWVAKIFG5Q4YJHUKM4S4NJZQIA3BAS6OJPK \
+    --in_amount $AQUA \
     --out_min $MIN_OUT | tee out
 
     [ \! -s out ] && exit
@@ -42,24 +41,24 @@ while [ $good = 1 ] ; do
     tok=`cat out`
     perl -e '
         $tok = int('$tok');
-        $an = '$BLND_N'+'$FEEMARGIN';
+        $an = '$AQUA_N'+'$FEEMARGIN';
         print("sell usdc > ".($an/($tok/10000000))."\n");
         print("buy '$TOKSTR' < ".(($tok/10000000)/$an)."\n");
     '
 
     tokv=`perl -e '$tok = int('$tok');print(($tok/10000000));'`
 
-    echo "sdex swap $tokv AQUA -> $BLND_N+$FEEMARGIN $TOKSTR"
+    echo "sdex swap $tokv AQUA -> $AQUA_N+$FEEMARGIN $TOKSTR"
     sleep 3
-    node sdex/index-blndaqua.js $tokv `echo $BLND_N+$FEEMARGIN|bc` | tee out2
+    node sdex/index-blndaqua.js $tokv `echo $AQUA_N+$FEEMARGIN|bc` inverse | tee out2
     if [ $PIPESTATUS = 0 ] ; then
         srcv=`cat out2`
     else
         srcv=0
     fi
 
-    echo "$BLND_N+$FEEMARGIN < $srcv ?"
-    if perl -e "($BLND_N+$FEEMARGIN)<$srcv"'&&exit(0)||exit(1)' ; then
+    echo "$AQUA_N+$FEEMARGIN < $srcv ?"
+    if perl -e "($AQUA_N+$FEEMARGIN)<$srcv"'&&exit(0)||exit(1)' ; then
         echo yes, continuing
     else
         echo no, stopping
