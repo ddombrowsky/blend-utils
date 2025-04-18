0554d1c16507e51007415736be906b6eb19c0a05
David Dombrowsky <davek@6thstreetradio.org>
2025-04-18 08:27:38 -0400

    added lookup table for asset ids

--- balances/index.ts
+++ balances/index.ts
@@ -15,15 +15,29 @@ const network: Network = {
   passphrase: "Public Global Stellar Network ; September 2015",
 };
 
+const assetLookup = {
+  'CAS3J7GYLGXMF6TDJBBYYSE3HQ6BBSMLNUQ34T6TZMYMW2EVH34XOWMA': 'XLM',
+  'CDTKPWPLOURQA2SGTKTUQOWRCBZEORB4BWBOMJ3D3ZTQQSGE5F6JBQLV': 'ERUC',
+  'CCW67TSZV3SSS2HXMBQ5JFGCKJNXKZM7UQUWUZPUTHXSTZLEO7SJMI75': 'USDC',
+  'CB226ZOEYXTBPD3QEGABTJYSKZVBP2PASEISLG3SBMTN5CE4QZUVZ3CE': 'USDGLO',
+  'CBLV4ATSIWU67CFSQU2NVRKINQIKUZ2ODSZBUJTJ43VJVRSBTZYOPNUR': 'USTRY',
+};
+
 const poolContracts = [
   {'id':'CDVQVKOY2YSXS2IC7KN6MNASSHPAO7UN2UR2ON4OI2SKMFJNVAMDX6DP','ver':0},
   {'id':'CBP7NO6F7FRDHSOFQBT2L2UWYIZ2PU76JKVRYAQTG3KZSQLYAOKIF2WB','ver':0},
-  {'id':'CAJJZSGMMM3PD7N33TAPHGBUGTB43OC73HVIK2L2G6BNGGGYOSSYBXBD','ver':1}
+  {'id':'CAJJZSGMMM3PD7N33TAPHGBUGTB43OC73HVIK2L2G6BNGGGYOSSYBXBD','ver':1},
+  {'id':'CCCCIQSDILITHMM7PBSLVDT5MISSY7R26MNZXCX4H7J5JQ5FPIYOGYFS','ver':1},
 ];
 const backstopContract = [
   'CAO3AGAMZVRMHITL36EJ2VZQWKYRPWMQAPDQD5YEOF3GIF7T44U4JAL3',
   'CAQQR5SWBXKIGZKPBZDH3KM5GQ5GUTPKB7JAFCINLZBC5WXPJKRG3IM7'
 ];
+
+function getSymbol(assetId: string) : string {
+  let sym = assetLookup[assetId];
+  return sym ? sym : assetId;
+}
 
 const replacer = (key: any, value: any) => {
   if (value instanceof Map) {
@@ -67,7 +81,7 @@ async function display(userPub: string, poolId: string, version: number) {
 
   console.log('  Liabilities ->');
   for (let reserve of pool.reserves.values()) {
-    let sym = reserve.assetId;
+    let sym = getSymbol(reserve.assetId);
     let val = pool_user.getLiabilitiesFloat(reserve);
     if (val > 0) {
       console.log(`\t${sym}:\t-${val}`);
@@ -76,7 +90,7 @@ async function display(userPub: string, poolId: string, version: number) {
 
   console.log('  Collateral ->');
   for (let reserve of pool.reserves.values()) {
-    let sym = reserve.assetId;
+    let sym = getSymbol(reserve.assetId);
     let val = pool_user.getCollateralFloat(reserve);
     if (val > 0) {
       console.log(`\t${sym}:\t${val}`);
